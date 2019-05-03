42bea4ccb59d8d0bb966037b58360e7b12dac1ac
Albert Kottke <albert.kottke@gmail.com>
2019-05-03 13:17:58 -0700

    Fixed name of header.

--- src/random.f
+++ src/random.f
@@ -81,7 +81,7 @@ c     Get random number
 c ----------------------------
       subroutine GetRandom2 ( iseed, n, wt, i1, i2, iSave, n1, n2, name )
 
-      include 'tornado.H'
+      include 'tornado.h'
 
       real wt(n1, n2, MAXPARAM), x
       integer n1, n2, n, iseed, i1, i2
@@ -109,7 +109,7 @@ c     Get random number
 c ----------------------------
       subroutine GetRandom3 ( iseed, n, wt, i1, i2, i3, iSave, n1, n2, n3 )
 
-      include 'tornado.H'
+      include 'tornado.h'
 
       real wt(n1, n2, n3, MAXPARAM), x
       integer n1, n2, n3
